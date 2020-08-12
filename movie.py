56ad3195468d36b871c4379ac2747583980d16d1
Nicolas Lair <contact@nicolaslair.fr>
2020-08-12 13:19:09 +0200

    method add_to_movies

--- movie.py
+++ movie.py
@@ -1,5 +1,6 @@
 import os
 import json
+import logging
 
 CUR_DIR = os.path.dirname(__file__)
 DATA_FILE = os.path.join(CUR_DIR, "data", "movies.json")
@@ -12,12 +13,27 @@ class Movie:
     def __str__(self):
         return self.title
 
-    def _get_movies(self):
+    @staticmethod
+    def _get_movies():
         with open(DATA_FILE, "r") as f:
             return json.load(f)
 
-    def _write_movies(self, movies):
+    @staticmethod
+    def _write_movies(movies):
         with open(DATA_FILE, "w") as f:
             json.dump(movies, f, indent=4)
 
+    def add_to_movies(self):
+        movies = self._get_movies()
+        if self.title not in movies:
+            movies.append(self.title)
+            self._write_movies(movies)
+            return True
+        else:
+            logging.warning(f'Le film {self.title} est déjà dans la liste')
+            return False
 
+
+if __name__ == '__main__':
+    m = Movie("Harry Potter")
+    m.add_to_movies()
