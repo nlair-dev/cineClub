c341c814328eedaa37db0694b9dfe991218f33d9
Nicolas Lair <contact@nicolaslair.fr>
2020-08-12 12:52:38 +0200

    add method _get_movies and _write_movies

--- movie.py
+++ movie.py
@@ -1,6 +1,23 @@
+import os
+import json
+
+CUR_DIR = os.path.dirname(__file__)
+DATA_FILE = os.path.join(CUR_DIR, "data", "movies.json")
+
+
 class Movie:
     def __init__(self, title):
         self.title = title.title()
 
     def __str__(self):
         return self.title
+
+    def _get_movies(self):
+        with open(DATA_FILE, "r") as f:
+            return json.load(f)
+
+    def _write_movies(self, movies):
+        with open(DATA_FILE, "w") as f:
+            json.dump(movies, f, indent=4)
+
+
